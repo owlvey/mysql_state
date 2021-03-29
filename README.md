632a945280a325b56bbe8300455ed9939b98cb2e
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-03-29 10:52:55 -0500

    add

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mysql_state
 mysql database for storage
+
+## connect dbearer
+
+https://stackoverflow.com/questions/50387952/how-to-resolve-unable-to-load-authentication-plugin-caching-sha2-password-issu
+
+
+https://community.atlassian.com/t5/Confluence-questions/MySQL-Public-Key-Retrieval-is-not-allowed/qaq-p/778956
---
+++ development.ps1
@@ -0,0 +1,7 @@
+docker run -d --rm --name owlvey-mysql -e MYSQL_ROOT_PASSWORD=thefalcon123 -e MYSQL_DATABASE=owlveydb  -p 3306:3306 mysql:8.0.23
+sleep 20
+flyway -configFiles="local.conf" info
+flyway -configFiles="local.conf" clean
+flyway -configFiles="local.conf" info
+flyway -configFiles="local.conf" migrate
+flyway -configFiles="local.conf" info
