a553ee864958df6d30d0b8b0dacec0ed4bfefdc0
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-03-30 15:24:10 -0500

    add

--- development.ps1
+++ development.ps1
@@ -1,3 +1,5 @@
+docker stop owlvey-mysql
+sleep 10
 docker run -d --rm --name owlvey-mysql -e MYSQL_ROOT_PASSWORD=thefalcon123 -e MYSQL_DATABASE=owlveydb  -p 3306:3306 mysql:8.0.23
 sleep 20
 flyway -configFiles="local.conf" info
